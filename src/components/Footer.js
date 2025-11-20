4a05206fb9220478b894efc2df7e541cc48230c0
ag <ag@ag.co.au>
2025-11-20 11:56:22 +0530

    feat: create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 PageLock. All Rights Reserved <a href="https://github.com/goyal-aman">amangoyal</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
